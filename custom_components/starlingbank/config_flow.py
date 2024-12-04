fc2b7344c2a8e7b278203688a3079d05a8af6947
Scott Jones <scott.jones@sky.uk>
2024-12-04 14:13:34 +0000

    feat: update to allow multiple instances

--- custom_components/starlingbank/config_flow.py
+++ custom_components/starlingbank/config_flow.py
@@ -1,81 +1,39 @@
 """Config flow for Starling Bank integration."""
-import logging
-
 import voluptuous as vol
 
-from homeassistant import config_entries, core, exceptions
-from homeassistant.const import CONF_TOKEN
-from homeassistant.core import callback
+from homeassistant.const import CONF_TOKEN, CONF_NAME
 import homeassistant.helpers.config_validation as cv
+from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 
 from .const import (
     DOMAIN,
 )
 
-_LOGGER = logging.getLogger(__name__)
-
-STEP_USER_DATA_SCHEMA = vol.Schema(
-    {
-        vol.Required(CONF_TOKEN): str,
-    }
-)
-
-async def validate_api(hass: core.HomeAssistant, data):
-    """Validate the credentials."""
-
-    return {"title": "Starling", "token": data[CONF_TOKEN]}
-
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+class StarlingConfigFlow(ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Starling Bank."""
 
     VERSION = 1
 
-    async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle a flow initialized by the user."""
         errors = {}
-        if user_input is None:
-            return self.async_show_form(
-                step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
-            )
 
-        self._async_abort_entries_match({CONF_TOKEN: user_input[CONF_TOKEN]})
-
-        try:
-            info = await validate_api(self.hass, user_input)
-        except CannotConnect:
-            errors["base"] = "cannot_connect"
-        else:
+        if user_input is not None:
             return self.async_create_entry(
-                title=info["title"], data=user_input
+                title=user_input[CONF_NAME], data=user_input
             )
+
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(CONF_TOKEN): str,
+                    vol.Optional(
+                        CONF_NAME, default=self.hass.config.location_name
+                    ): str,
+                }
+            ),
+            errors=errors,
         )
-
-
-class CannotConnect(exceptions.HomeAssistantError):
-    """Error to indicate we cannot connect."""
-
-
-class InvalidAuth(exceptions.HomeAssistantError):
-    """Error to indicate there is invalid auth."""
-
-
-class InvalidSecret(exceptions.HomeAssistantError):
-    """Error to indicate auth failed due to invalid secret."""
-
-
-class InvalidKey(exceptions.HomeAssistantError):
-    """Error to indicate auth failed due to invalid key."""
-
-
-class AlreadyConfigured(exceptions.HomeAssistantError):
-    """Error to indicate Coinbase API Key is already configured."""
-
-
-class CurrencyUnavailable(exceptions.HomeAssistantError):
-    """Error to indicate the requested currency resource is not provided by the API."""
-
-
-class ExchangeRateUnavailable(exceptions.HomeAssistantError):
-    """Error to indicate the requested exchange rate resource is not provided by the API."""
